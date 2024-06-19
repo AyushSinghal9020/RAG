af71ec8ac496d4486cd7906d9ab833d1849e54b1
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2024-06-19 10:35:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,59 @@
-import streamlit as st
 import PyPDF2
-from langchain.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
+
+import streamlit as st
+
+import google.generativeai as genai
+
+from gradio_client import Client , file
+from pdf2image import convert_from_path
+from langchain.schema.document import Document
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_core.messages import HumanMessage
 from langchain_cohere import ChatCohere
 
-file = st.file_uploader('Upload a PDF file' , type = ['pdf'])
+file = st.file_uploader('Upload file' , type = ['pdf'])
+query = st.text_input('Search Query')
 
-if file is not None : 
+
+if file : 
+
+    documents = []
+
+    open('file.pdf', 'wb').write(file.getbuffer())   
+
+    images = convert_from_path('file.pdf')
+
+    genai.configure(api_key = 'AIzaSyCQdd-2-hoFLiSxIZaIYQkCyipRR4_z51g')
+
+    model = genai.GenerativeModel('gemini-pro-vision')
+
+
+    with st.spinner('Generating Image Captions ') : 
+
+
+        for image in images :         
+
+            image.save(f'image.png' , 'PNG')
+            try : 
+
+                response = model.generate_content(
+                    [
+                        '' , 
+                        image
+                    ])
+            
+                response = response.text
+
+            except : response = 'Couldnt generate caption for this image'
+
+            documents.append(Document(
+                page_content = response , 
+                metadata = {
+                    'type' : 'image' , 
+                    'image' : image
+                }
+            ))
 
     pdf = PyPDF2.PdfReader(file)
 
@@ -23,39 +69,53 @@ if file is not None :
         in range(0 , len(text) , 1024)
     ]
 
-    chat = ChatCohere(cohere_api_key = 'FELFXgLGfcqsy4eh4Q75dXNT7VyIQjKZmhkiIug3')
-    embeddings = HuggingFaceEmbeddings(model_name = 'all-MiniLM-L6-v2')
-    vectorstore = FAISS.from_texts(chunks , embeddings)
+    for chunk in chunks : 
 
-    query = st.text_input('Enter your query')
+        documents.append(
+            Document(
+                page_content = chunk , 
+                metadata = {'type' : 'text'}
+            ))
 
-    if st.button('Search') :
+    with st.spinner('Creating Vector Store') :
 
-        similar_docs = vectorstore.similarity_search(query)
-        context = ' '.join([
-            similar_doc.page_content
-            for similar_doc
-            in similar_docs
-        ])
+        embeddings = HuggingFaceEmbeddings(model_name = 'all-MiniLM-L6-v2')
 
-        prompt = '''
+        vc = FAISS.from_documents(
+            documents ,  
+            embedding = embeddings
+        )
+
+    if query : 
+
+        with st.spinner('Searching ') : 
+
+            similar_docs = vc.similarity_search(query , 5)
+
+            context = ''
+            for doc in similar_docs : 
+
+                if doc.metadata['type'] == 'image' : 
+
+                    st.image(doc.metadata['image'])
+                    
+                    context += doc.page_content
+
+                else : context += doc.page_content
+
+            prompt = '''
 You are a conversational chatbot, your task is to answer questions based on the context provided.
-
-Try to answer in markdown format. to format, images or any links as well
-
-Your code will be directly sent to a markdown renderer
 
 Context : {}
 
 Query : {}
-        '''
+            '''
 
-        prompt = prompt.format(context , query)
+            chat = ChatCohere(cohere_api_key = 'FELFXgLGfcqsy4eh4Q75dXNT7VyIQjKZmhkiIug3')
+            prompt = prompt.format(context , query)
 
-        messages = [HumanMessage(content = prompt)]
+            messages = [HumanMessage(content = prompt)]
+            
+            response = chat.invoke(messages).content
 
-        response = chat.invoke(messages).content
-
-        open('file.txt' , 'w').write(response)
-
-        st.markdown(response)
+            st.write(response)
